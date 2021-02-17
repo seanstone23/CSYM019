b06c33513a52364bbf2f2cf468ab0e7230f7584a
Sean Stone <P17207062@my365.dmu.ac.uk>
2021-02-17 16:26:07 +0000

    excercise 4

---
+++ Week4/excercise 4/script.js
@@ -0,0 +1,9 @@
+function loadFunction(){
+var element = document.getElementById('myTitle');
+element.firstChild.nodeValue = 'New Heading';
+
+var element = document.getElementById('myPara');
+element.firstChild.nodeValue = 'New Paragraph';
+}
+
+document.addEventListener('click', loadFunction);
