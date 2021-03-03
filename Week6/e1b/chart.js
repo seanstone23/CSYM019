0b058b16afae823614680610165d410637ecf4ac
Sean Stone <P17207062@my365.dmu.ac.uk>
2021-03-03 15:45:49 +0000

    bar chart and pie chart version added

---
+++ Week6/e1b/chart.js
@@ -0,0 +1,29 @@
+var element = document.getElementById('myChart').getContext('2d');
+
+
+var chartType = 'pie';
+
+   // The data for our dataset
+   var chartData = {
+       labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+       datasets: [{
+           label: 'My First dataset',
+           backgroundColor: 'rgb(255, 99, 132)',
+           borderColor: 'rgb(255, 99, 132)',
+           data: [0, 10, 5, 2, 20, 30, 45]
+       }]
+   };
+
+   // Configuration options go here
+   var chartOptions = {
+     layout: {
+           padding: {
+               left: 50,
+               right: 0,
+               top: 0,
+               bottom: 0
+           }
+         }
+   };
+
+var chart = new Chart(element, {type: chartType, data: chartData, options: chartOptions});
